faf32a073351bfb8f703eef6bcaa0329505c890f
DarushDev <darushfaqiri@gmail.com>
2017-08-26 13:24:34 +0530

    Add Fragment classes

---
+++ app/src/main/java/com/example/myandroidfragment/RageComicDetailsFragment.java
@@ -0,0 +1,24 @@
+package com.example.myandroidfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//1
+public class RageComicDetailsFragment extends Fragment {
+    //2
+    public static RageComicDetailsFragment newInstance() {
+        return new RageComicDetailsFragment();
+    }
+
+    //3
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rage_comic_details, container, false);
+    }
+
+}
